3c842fde3bf665dbdd7e7393b0fc9442005d9c9a
lars20070 <l.nilse@dunelm.org.uk>
2017-06-18 13:30:19 +0200

    add script for generating reports from the PSM section of mzTab

---
+++ mzTabPSM2report/misc/mzTabPSM2report.R
@@ -0,0 +1,167 @@
+## This is an R script for the conversion of mzTab to a better readable tsv format
+
+# options
+options(digits=10)
+breaks = 80
+
+input.file <- 'example.mzTab'
+
+# count the occurences of character c in string s
+countOccurrences <- function(c,s) {
+  s2 <- gsub(c,"",s)
+  return (nchar(s) - nchar(s2))
+}
+
+# check that all protein accessions are of the format *|*|* 
+checkAccessionFormat <- function(accessions) {
+  n <- length(accessions)
+  count <- countOccurrences("[|]",accessions)
+  m <- length(which(count==2))
+  return (n==m)
+}
+
+# collapse rows
+# (In mzTab, PSMs with multiple protein accessions are reported in multiple rows. This function collapses them to a single row.)
+collapseRows <- function(psm.data) {
+  
+  # generate index vector idx
+  tmp.psm.id <- 0
+  idx <- c()
+  accessions.tmp <- c()
+  accessions.strings <- c()
+  for (i in 1:length(psm.data$PSM_ID)) {
+    
+    if (psm.data$PSM_ID[i] == tmp.psm.id) {
+      if (length(accessions.tmp) > 0) {
+        accessions.strings <- c(accessions.strings, toString(accessions.tmp, sep=','))
+        accessions.tmp <- c()
+      }
+      
+      idx <- c(idx,i)
+      tmp.psm.id <- tmp.psm.id + 1
+    }
+    
+    accessions.tmp <- c(accessions.tmp, psm.data$accession[i])
+  }
+  accessions.strings <- c(accessions.strings, toString(accessions.tmp, sep=','))
+  
+  psm.data <- psm.data[idx,]
+  psm.data$accession <- accessions.strings
+  
+  return (psm.data)
+}
+
+# read the PSM section of an mzTab file
+readMzTabPSM <- function(file) {
+  
+  # read entire mzTab
+  no.col <- max(count.fields(file, sep = "\t", quote=""))
+  data <- read.table(file, sep="\t", fill=TRUE, quote="", col.names=1:no.col)
+  
+  # extract PSM data
+  psm.data <- data[which(data[,1]=="PSM"),]
+  colnames(psm.data) <- unlist(data[which(data[,1]=="PSH")[1],])
+  psm.data$PSH <- NULL
+  
+  # simplify accession (in case it is of the format *|*|* )
+  psm.data$accession <- as.character(psm.data$accession)
+  if (checkAccessionFormat(psm.data$accession)) {
+    list <- strsplit(psm.data$accession, "[|]")
+    psm.data$accession <- unlist(lapply(list, '[[', 2))
+    psm.data$gene <- unlist(lapply(list, '[[', 3))
+  }
+  
+  psm.data <- collapseRows(psm.data)
+  
+  return (psm.data)
+}
+
+# check whether the column search_engine_score[n] exists
+checkSearchEngineScoreExists <- function(table, n) {
+  column <- paste("search_engine_score[", as.character(n), "]", sep="")
+  return (column %in% colnames(table))
+}
+
+# check whether the column opt_global_SpecEValue_score exists
+checkEValueExists <- function(table) {
+  column <- "opt_global_SpecEValue_score"
+  return (column %in% colnames(table))
+}
+
+# plot score distribution
+plotScoreDistribution <- function(scores, pdf.file, breaks)
+{
+  if (is.factor(scores))
+  {
+    scores <- as.numeric(as.character(scores))
+  }
+  
+  pdf(file=pdf.file, height=4)
+  hist(log10(scores), xlab=expression('log'[10]*' score'), ylab="frequency", freq=TRUE, main="", col="grey", breaks=breaks)
+  dev.off()
+}
+
+
+
+psm.data <- readMzTabPSM(input.file)
+
+n.total <- dim(psm.data)[1]
+n.unique <- length(which(psm.data$unique==1))
+n.nonredundant <- length(unique(psm.data$sequence))
+n.unique.nonredundant <- length(unique(psm.data[which(psm.data$unique==1),]$sequence))
+
+n.target <- length(which(psm.data$opt_global_target_decoy=="target"))
+n.decoy <- length(which(psm.data$opt_global_target_decoy=="decoy"))
+n.target.decoy <- length(which(psm.data$opt_global_target_decoy=="target+decoy"))
+n.target.nonredundant <- length(unique(psm.data[which(psm.data$opt_global_target_decoy=="target"),]$sequence))
+n.decoy.nonredundant <- length(unique(psm.data[which(psm.data$opt_global_target_decoy=="decoy"),]$sequence))
+n.target.decoy.nonredundant <- length(unique(psm.data[which(psm.data$opt_global_target_decoy=="target+decoy"),]$sequence))
+
+if (checkSearchEngineScoreExists(psm.data, 1))
+{
+  scores <- psm.data$`search_engine_score[1]`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__search_engine_score.pdf", breaks)
+  }
+  
+  scores <- psm.data[which(psm.data$opt_global_target_decoy=="target"),]$`search_engine_score[1]`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__search_engine_score__target.pdf", breaks)
+  }
+  
+  scores <- psm.data[which(psm.data$opt_global_target_decoy=="decoy"),]$`search_engine_score[1]`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__search_engine_score__decoy.pdf", breaks)
+  }
+}
+
+if (checkEValueExists(psm.data))
+{
+  scores <- psm.data$`opt_global_SpecEValue_score`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__e_value_score.pdf", breaks)
+  }
+  
+  scores <- psm.data[which(psm.data$opt_global_target_decoy=="target"),]$`opt_global_SpecEValue_score`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__e_value_score__target.pdf", breaks)
+  }
+  
+  scores <- psm.data[which(psm.data$opt_global_target_decoy=="decoy"),]$`opt_global_SpecEValue_score`
+  
+  if (length(scores) > 0)
+  {
+    plotScoreDistribution(scores, "plot__e_value_score__decoy.pdf", breaks)
+  }
+}
+
