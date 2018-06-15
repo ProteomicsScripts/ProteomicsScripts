a9ac6e8960095389270fb7a1e711d0dcbc4148f6
MFreidank <freidankm@yahoo.de>
2018-06-15 16:22:13 +0200

    chg: added mzTab2DoubletTailsMultipleSpeciesreport
    supports an arbitrary amount of species,
    defined as a list of lists, where each sublist
    has two elements (MARKER, TAXONOMY_ID)
    that identifies a MARKER to match for the species
    in `data$gene` and a UniProt TAXONOMY_ID
    for this species.

---
+++ mzTab2DoubletTAILSMultipleSpeciesReport/mzTab2DoubletTAILSMultipleSpeciesreport.sh
@@ -0,0 +1,50 @@
+# Script to create duplex report from .consensusXML results
+
+#!/bin/sh
+
+# script directory
+SCRIPT_PATH=$(dirname -- "$(readlink -f -- "$0")")
+
+if [[ $1 == "" ]]
+then
+echo "Please specify a file."
+exit
+fi
+
+# input file
+FILE=$1; shift
+FILE_ABSOLUTE=$(readlink -f -- $FILE)
+FILE_PATH=$(dirname $FILE_ABSOLUTE)
+FILE_BASE=$(basename $FILE_ABSOLUTE)
+FILE_NAME=${FILE_BASE%.*}
+
+if ! [[ -f $FILE ]]
+then
+echo "File does not exist."
+exit
+fi
+
+echo 'Generating TAILS report from mzTab file '$FILE_ABSOLUTE'.'
+cd $SCRIPT_PATH
+
+# copy mzTab
+cp $FILE_ABSOLUTE data.mzTab
+
+# replace dummy by file name
+sed -e 's/FILE_NAME_DUMMY/'$FILE_NAME'/g' mzTab2DoubletTAILSreport.Snw > mzTab2DoubletTAILSreport_temp.Snw
+
+# Run the R code
+R -e "Sweave('mzTab2DoubletTAILSreport_temp.Snw')"
+
+pdflatex mzTab2DoubletTAILSreport_temp.tex
+
+mv mzTab2DoubletTAILSreport_temp.pdf $FILE_PATH/$FILE_NAME.pdf
+mv data.tsv $FILE_PATH/$FILE_NAME.tsv
+
+# clean-up
+rm data*
+rm FcLogIntensity*
+rm frequency*
+rm mzTab2DoubletTAILSreport_temp.*
+
+cd $CURRENT_PATH
