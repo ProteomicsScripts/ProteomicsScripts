78b78a4ca7d7da18b9f7753c6904d1576385f4b8
MFreidank <freidankm@yahoo.de>
2018-07-05 15:55:32 +0200

    lots of documentation

--- MQ2mzTab/misc/MQ2mzTab.R
+++ MQ2mzTab/misc/MQ2mzTab.R
@@ -1,8 +1,9 @@
+## TODO: Change docu here.
 ## This is an R script for the conversion of mzTab to a better readable tsv format
-## To install dependencies, run in R:
-## install.packages(devtools)
-## install.packages(tidyr)
 
+# NOTE: modifications missing 
+
+library("methods")  # required to avoid a warning from R
 library("tidyr")
 
 # clear entire workspace
@@ -24,7 +25,7 @@ END_COLOR_ANSI = "\033[0m"
 #' Required files for processing are: allPeptides.txt 
 
 #' @param folder Character, path to a folder on disk that should contain MaxQuant results.
-#' @param relevant_files List of files that are necessary for conversion to \code{mzTab}.
+#' @param relevant_files Character List, Files that are necessary for conversion to \code{mzTab}.
 checkMaxQuantFolder <- function(folder, relevant_files=c("allPeptides.txt")) {
     if (!dir.exists(file.path(folder))) {
         message = sprintf("Folder at path '%s' not found.", input.folder)
@@ -39,49 +40,57 @@ checkMaxQuantFolder <- function(folder, relevant_files=c("allPeptides.txt")) {
     }
 }
 
-separateRows <- function(dataframe, col, sep) {
-    rest_of_dataframe = dataframe[, -which(names(dataframe) == col)]
-    # print(rep(rest_of_dataframe, 2))
-    return (dataframe)
+
+#' Determine type of analysis used to obtain our MaxQuant input files.
+#' 
+#' Supported types of analyses: \code{"TMT"}, \code{"Labeled"}
+#' A TMT analysis is expected to have a column with name \code{"Reporter.intensity"}.
+#' A Labeled analysis is expected to have a column with name \code{"Intensity.H"} 
+#' and a column with name \code{Intensity.L}.
+#' 
+#' @param column_names List of Characters, Names of all columns of MaxQuant's \code{allPeptides.txt}.
+
+analysisType <- function(column_names) {
+    if (any(grepl("Reporter.intensity", column_names))) {
+        return("TMT")
+    } else if (any(grepl("Intensity.L", column_names)) && 
+               any(grepl("Intensity.H", column_names)))
+    {
+        return("Labeled")
+    }
+    message = "Unknown type of analysis.\nTMT analyses are expected to have a column named 'Reporter intensity'.\nLabeled analyses are expected to have at least a column named 'Intensity.L' and a column named 'Intensity.H'."
+    stop(sprintf("%s%s%s", FAIL_COLOR_ANSI, message, END_COLOR_ANSI))
 }
 
-# generate a PEP section from files in `maxquant_folder`.
-# The following columns are considered necessary:
-## sequence = peptide sequence
-## accession 
-## peptide abundance_[1 -- n] abundances for each of 1..n study variables
-## RT
-## mz
-## charge
-
 # Each row is one (in mzTab potentially non-unique) detected peptide.
-# NOTE: Check how modifications are represented in maxquant => 
+#' Generate a \code{mzTab} PEP section from the given \code{max_quant_peptides}. 
+#'
+#' \code{max_quant_peptides} are expected to have a column-format like in MaxQuant's 
+#' \code{allPeptides.txt}. 
+#'
+#' @param max_quant_peptides data.frame, Peptides parsed from MaxQuant's \code{allPeptides.txt}, possibly with semicolon separated \code{"Proteins"} column for each peptide. 
 generatePEP<- function(max_quant_peptides) {
+
+  # Determine type of analysis used.
   column_names = sort(colnames(max_quant_peptides))
+  analysis = analysisType(column_names=column_names)
 
-  # max_quant_peptides = separate_rows(max_quant_peptides, 
-  #                                    col="Proteins", 
-  #                                    sep=";", 
-  #                                    convert=TRUE)
+  # use tidyr magic to split a row with semicolon-separated column "Proteins" into 
+  # multiple rows that each have a single protein 
+  # (and the same values in all remaining columns).
+  max_quant_peptides = separate_rows(max_quant_peptides, col="Proteins", sep=";")
 
-  # rest_of_dataframe = max_quant_peptides[, -which(names(max_quant_peptides) == "Proteins")]
-  # proteins_split = strsplit(max_quant_peptides$Proteins, ";")
-  # duplicated = rep(rest_of_dataframe$, sapply(proteins_split, length))
-  # max_quant_peptides = data.frame(unlist(proteins_split), rest_of_dataframe)
-  max_quant_peptides = separateRows(max_quant_peptides, col="Proteins", sep=";")
-
-  # Check which type of analysis this is.
-  is_tmt = any(grepl("Reporter.intensity", column_names))
-  is_labeled = any(grepl("Intensity.L", column_names)) && any(grepl("Intensity.H", column_names))
-  mztab_column_names = c(
-    "PEH", "sequence", "accession", "unique", "database",	
+  # NOTE: The code below this is quite lengthy and can probably be improved.
+  mztab_column_names = c("PEH", "sequence", "accession", "unique", "database",	
     "database_version", "search_engine", "best_search_engine_score[1]", 
     "search_engine_score[1]_ms_run[1]", "modifications", "retention_time",
     "retention_time_window",	
-    "charge",	"mass_to_charge	spectra_ref")
+    "charge",	"mass_to_charge	spectra_ref"
+  )
 
 
-  if (is_labeled) {
+  if (analysis == "Labeled") {
+      # Results stem from TMT analysis.
       mztab_column_names = c(mztab_column_names, 
                              "peptide_abundance_study_variable[1]", 
                              "peptide_abundance_stdev_study_variable[1]", 
@@ -106,7 +115,7 @@ generatePEP<- function(max_quant_peptides) {
                           nulls, max_quant_peptides["Charge"], nulls, 
                           max_quant_peptides["Intensity.L"], nulls, nulls,
                           max_quant_peptides["Intensity.H"], nulls, nulls,
-                          max_quant_peptides["Intensity.M"], nulls, nulls)
+                          max_quant_peptides["Intensity.M"], nulls, nulls,)
       } else {
           df = data.frame(rep("PEP", nrow(max_quant_peptides)), 
                           max_quant_peptides["Sequence"], 
@@ -122,20 +131,14 @@ generatePEP<- function(max_quant_peptides) {
                           max_quant_peptides["Intensity.L"], nulls, nulls,
                           max_quant_peptides["Intensity.H"], nulls, nulls)
       }
-  } else if (is_tmt) {
+  } else if (analysis == "TMT") {
       # XXX: handle tmt, reporter.intensity seems to be how peptide_abundance_study_variables are represented here.
       stop("It seems as if your input is a TMT analysis. Currently only labeled analyses are supported!")
   } else {
       stop("Unsupported type of analysis.\nExpected TMT analysis (with at least one column starting with 'Reporter intensity') or labeled analysis (with at least columns 'Intensity H', 'Intensity L').")
   }
 
-  print(is_tmt)
-  print(is_labeled)
-  # maxquant lists matching proteins in a ";" seperated string for each peptide
-  # => mzTab expects a single protein accession per listed peptide, 
-  # so we need to seperate each row whose "Proteins" column lists multiple proteins
-  # into many rows with one protein each.
-  # NOTE: mz vs uncalibrated mz?
+  # Overwrite column names with correct ones.
   colnames(df) <- mztab_column_names
 
   # replace NA with "null" (as this is the 'missing information marker' for mzTab)
@@ -143,6 +146,15 @@ generatePEP<- function(max_quant_peptides) {
   return (df)
 }
 
+#' Determine an output filename from the `Raw file` column of the MaxQuant inputs.
+#'
+#' If there is a single unique filename in the `Raw file` column of the MaxQuant inputs, 
+#' the output filename is exactly that unique filename with suffix \code{".mzTab"}. 
+#' If there are multiple filenames in the `Raw file` column of the MaxQuant inputs, 
+#' the output filename is \code{RAWFILE_etal.mzTab} where \code{RAWFILE} is 
+#' the name of the first filename in the `Raw file` column of the MaxQuant inputs.
+#'
+#' @param input_files Character List, Filenames present in the `Raw file` column of \code{allPeptides.txt} of MaxQuant.
 outputFilename<- function(input_files) {
     single_input_file = length(unique(input_files)) == 1
     first_input_file = input_files[[1]]
@@ -152,6 +164,13 @@ outputFilename<- function(input_files) {
     return (paste(first_input_file, "_etal.mzTab", sep=""))
 }
 
+#' Write a header for a resulting \code{mzTab} file.
+#'
+#' Header has information about information source (\code{uri}) and 
+#' \code{mzTab} metadata.
+#'
+#' @param uri Character, Name of input folder containing MaxQuant results.
+#' @param maintainer Character, Description of current maintainer of this tool.
 mzTabHeader <- function(uri, maintainer="Moritz Freidank, freidankm@gmail.com") {
     return (c("MTD\tmzTab-version\t1.0.0", "MTD\tmzTab-mode\tSummary",
                "MTD\tmzTab-type\tQuantification", "MTD\tdescription\tGenerated using MQ2mzTab.R from MaxQuant Output",
@@ -159,10 +178,10 @@ mzTabHeader <- function(uri, maintainer="Moritz Freidank, freidankm@gmail.com") 
                "MTD\tpeptide_search_engine_score[1]\tnull",
                "MTD\tpsm_search_engine_score[1]\tnull",
                paste("MTD\turi[1]\t", uri)))
-
 }
 
 
+#  Parse folder name from command line and validate it. {{{ # 
 command_line_arguments <- commandArgs(trailingOnly = TRUE)
 
 if (length(command_line_arguments) != 1) {
@@ -172,7 +191,10 @@ if (length(command_line_arguments) != 1) {
 
 input.folder <- commandArgs(trailingOnly = TRUE)[[1]]
 checkMaxQuantFolder(input.folder)
+#  }}} Parse folder name from command line # 
 
+
+#  Generate PEP section {{{ # 
 
 f = file.path(input.folder, "allPeptides.txt")
 max_quant_peptides = read.table(f, sep="\t", header=TRUE, stringsAsFactors=FALSE)
@@ -180,13 +202,13 @@ max_quant_peptides = read.table(f, sep="\t", header=TRUE, stringsAsFactors=FALSE
 output_filename <- outputFilename(input_files=max_quant_peptides$Raw.file)
 
 pep_section <- generatePEP(max_quant_peptides)
+#  }}} Generate PEP section # 
 
-output_file <- file(output_filename, open="wt")
-on.exit(close(output_file))
 
-           # NOTE: modifications missing and maybe there is an equivalent of `ms_run[1]-location`?
     
 #  Write mzTab header to file {{{ # 
+output_file <- file(output_filename, open="wt")
+on.exit(close(output_file))
 
 # uri is the source used to generate a given mzTab file. 
 # In our context here it is 'MAXQUANT_RESULTS_FOLDER/allPeptides.txt'
@@ -196,9 +218,11 @@ cat(mzTabHeader(uri=uri), sep="\n", file=output_file)
 cat("\n", file=output_file)
 #  }}} Write mzTab header to file # 
 
-# Append PEP section.
+#  Append PEP section. {{{ # 
+
 write.table(pep_section, 
             file=output_file, 
             quote=FALSE, 
             sep="\t", 
             row.names=FALSE)
+#  }}} Append PEP section. # 
