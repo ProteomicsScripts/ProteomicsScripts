99e53887532d9e415f7329928d7a8d5f09b90d66
lars20070 <l.nilse@dunelm.org.uk>
2018-09-12 11:28:11 +0200

    update drag-n-drop script

--- mzTab2report/mzTab2report_dragNdrop.bat
+++ mzTab2report/mzTab2report_dragNdrop.bat
@@ -1,8 +1,5 @@
 @echo OFF
-ECHO.
-ECHO This batch file is used to invoke the mzTab2report script by drag'n'dropping of
-ECHO a mzTab file onto this .bat file.
-
+REM This batch file is used to invoke the mzTab2report script by drag'n'dropping of an mzTab file onto this .bat file.
 
 REM A usage message with multiline (i.e. keep the empty line!)
 setlocal EnableDelayedExpansion
@@ -15,7 +12,7 @@ Usage: ^
 Please try again.
 REM end of USAGE
 
-REM check number of arguments (must be 1 [mzTab file])
+REM Check number of arguments (must be 1, the mzTab file)
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 if %argC%==0 (
@@ -26,22 +23,17 @@ if %argC%==0 (
 
 REM Check upon first argument (the mzTab file)
 REM use bare %1 not "%1", it works fine, even with spaces
-REM if not exist %1 (
-REM   goto not_found
-REM ) 
+if not exist %1 (
+  ECHO The file %1 does not exist.
+  goto :end
+)
 
-REM ~dp (drive,path); the final '\' is required!; manual quoting required as well
-set MZTABFILE=%1
+REM We assume mzTab2report to be in the PATH.
+ECHO Generating PDF report for %1 by calling mzTab2report.
+start mzTab2report %1
 
-ECHO mzTab file is at '%MZTABFILE%'
+REM TO-DO: The above line opens a new terminal window. How to close it automatically?
 
-REM Execute 'mzTab2report.bat' script (which we assume to be in the PATH) with the given mzTab file.
-CALL mzTab2report.bat %MZTABFILE%
-
-
-REM :not_found
-REM echo Could not find the given (network) folder '%1'^^! Please contact your admin/bioinformatician^^!
-REM goto end
-
-REM :end
+:end
 REM pause
+exit
