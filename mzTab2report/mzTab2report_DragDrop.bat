242d65b1ecde81423bdac47db45c47df8b8cad3b
Lars Nilse <l.nilse@dunelm.org.uk>
2018-12-29 14:21:46 +0100

    new drag-n-drop script for Windows

---
+++ mzTab2report/mzTab2report_DragDrop.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+REM Script for executing mzTab2report by simply drag-n-drop an mzTab file to this batch script.
+
+REM Let us assume mzTab2report is in the $PATH.
+mzTab2report "%1"
+
+REM Let us assume mzTab2report is not in the $PATH.
+REM D:\data\Scripts\ProteomicsScripts\mzTab2report\mzTab2report.bat "%1"
