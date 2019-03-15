fa8c87f8b1b1214eb890fec9f8da9fd030e59cec
lars20070 <l.nilse@dunelm.org.uk>
2019-03-15 10:43:34 +0100

    register account

--- shiny/shiny.R
+++ shiny/shiny.R
@@ -1,11 +1,12 @@
-install.packages('rsconnect')
+# first shiny app
+# follow tutorial at https://www.shinyapps.io/admin/#/dashboard
+
+#install.packages('rsconnect')
 
 library(rsconnect)
 
-rsconnect::setAccountInfo(name='lars20070',
-                          token='AFD4219F214CFBAEE19AB4170DCC8343',
-                          secret='<SECRET>')
+#rsconnect::setAccountInfo(name='lars20070', token='AFD4219F214CFBAEE19AB4170DCC8343', secret='BXBlMEalP8WEzCErBjwCKhJ33LF8iUCuX9bDdbU0')
 
 rsconnect::deployApp('path/to/your/app')
 
-#https://www.shinyapps.io/admin/#/dashboard
+#http://shiny.rstudio.com/articles/shinyapps.html
